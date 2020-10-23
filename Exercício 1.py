a3612fcb1f3fba308c6ef9ab2059448dca4eef62
Bruna Cristina <btorquato70@gmail.com>
2020-10-23 09:31:06 -0300

    Adicionar exercicio 1

--- Exercício 1.py
+++ Exercício 1.py
@@ -37,7 +37,7 @@ peso = [
     3
 ]
 
-def corrigir_prova(lista_alunos, gabarito, peso, posicao_aluno):
+def calcular_nota(lista_alunos, gabarito, peso, posicao_aluno):
     aluno = lista_alunos[posicao_aluno]
     lista_respostas = aluno['respostas']
     nome_aluno = aluno['nome']
@@ -49,7 +49,7 @@ def corrigir_prova(lista_alunos, gabarito, peso, posicao_aluno):
             nota_aluno = nota_aluno + peso[contador_peso]
         contador_gabarito = contador_gabarito + 1
         contador_peso = contador_peso + 1
-    print(f'A nota do(a) aluno(a) {nome_aluno} foi {nota_aluno}')
+    print(f'A nota do(a) aluno(a) {nome_aluno} é {nota_aluno}')
 
-print(corrigir_prova(alunos, gabarito, peso, 0))
-print(corrigir_prova(alunos, gabarito, peso, 1))
+print(calcular_nota(alunos, gabarito, peso, 0))
+print(calcular_nota(alunos, gabarito, peso, 1))
